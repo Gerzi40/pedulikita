931ec9b815787e173a17fbb23284205c5eac4a24
Garry_nth <garry.nathanael@gmail.com>
2025-10-27 07:56:23 +0700

    route changes

--- resources/views/events/organization_show.blade.php
+++ resources/views/events/organization_show.blade.php
@@ -63,6 +63,21 @@
                             <span>{{ \Carbon\Carbon::parse($event->start_time)->format('H:i') }} –
                                 {{ \Carbon\Carbon::parse($event->end_time)->format('H:i') }} WIB</span>
                         </div>
+                        {{-- <div>
+                            @if ($event->state == 'approved')
+                                <span class="inline-flex items-center px-3 py-1 text-xs font-semibold rounded-full bg-green-600 text-white">
+                                    Disetujui
+                                </span>
+                            @elseif ($event->state == 'rejected')
+                                <span class="inline-flex items-center px-3 py-1 text-xs font-semibold rounded-full bg-red-600 text-white">
+                                    Ditolak
+                                </span>
+                            @else
+                                <span class="inline-flex items-center px-3 py-1 text-xs font-semibold rounded-full bg-[var(--color1)] text-white">
+                                    {{ ucfirst($event->state) }}
+                                </span>
+                            @endif
+                        </div> --}}
                     </div>
                 </div>
 
