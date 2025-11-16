5af0c4e5c86579231f1d22ed049278341990bade
Garry_nth <garry.nathanael@gmail.com>
2025-11-16 21:04:17 +0700

    Volunteer's follow function

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Organization;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -21,11 +22,23 @@ class FollowController extends Controller
     public function store(string $organization_id)
     {
 
-        
+        $organization = Organization::findOrFail($organization_id);
+        $user = Auth::user();
+        $volunteer_id = $user->volunteer->id;
+
+        if ($organization->volunteers()->where('volunteers.id', $volunteer_id)->first())
+        {
+            return redirect()->back()->with('error', 'Organisasi sudah diikuti');
+        }
+
+        $organization->volunteers()->attach($volunteer_id);
+        return back();
     }
 
     public function destroy(string $organization_id)
     {
-        
+        $user = Auth::user();
+        $user->volunteer->organizations()->detach($organization_id);
+        return back();
     }
 }
