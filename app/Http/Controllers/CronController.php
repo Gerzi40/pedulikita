c36b2af6b5d046cee87ad06e3b7b921e67c45a48
narutodatebayo1 <kwntimothy@gmail.com>
2025-12-04 20:24:02 +0700

    add populate volunteer point rating cron

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Organization;
+use App\Models\Volunteer;
+use App\Models\VolunteerPointRating;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 
@@ -11,5 +13,42 @@ class CronController extends Controller
     public function block_rejected_organization()
     {
         Organization::where('rejected_at', '<=', Carbon::now()->subMonths(1))->where('state', '=', 'rejected')->update(['state' => 'blocked']);
+    }
+
+    public function populate_volunteer_point_rating()
+    {
+        $now = Carbon::now();
+
+        if ($now->month == 12)
+        {
+            $year = $now->year + 1;
+            $month = 1;
+        }
+        else
+        {
+            $year = $now->year;
+            $month = $now->month + 1;
+        }
+
+        $volunteer_ids = Volunteer::pluck('id');
+        $volunteer_point_rating_ids = VolunteerPointRating::where('year', '=', $year)
+            ->where('month', '=', $month)
+            ->pluck('volunteer_id');
+
+        $insert_volunteer_ids = $volunteer_ids->diff($volunteer_point_rating_ids);
+
+        $insert_data = [];
+
+        foreach ($insert_volunteer_ids as $volunteer_id) {
+            $insert_data[] = [
+                'volunteer_id' => $volunteer_id,
+                'year' => $year,
+                'month' => $month,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+        }
+
+        VolunteerPointRating::insert($insert_data);
     }
 }
--- routes/cron.php
+++ routes/cron.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\CronController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/cron/block-rejected-organization', [CronController::class, 'block_rejected_organization']);
+Route::get('/cron/populate-volunteer-point-rating', [CronController::class, 'populate_volunteer_point_rating']);
