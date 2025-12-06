7e81473cf3c767fb461b5c6dc548ce682373e96e
narutodatebayo1 <kwntimothy@gmail.com>
2025-12-06 08:58:07 +0700

    responsive event detail page

--- resources/views/events/guest_show.blade.php
+++ resources/views/events/guest_show.blade.php
@@ -65,21 +65,18 @@
     <section class="max-w-6xl mx-auto mt-12 px-4 space-y-6">
         <h1 class="text-2xl md:text-3xl font-bold text-gray-800">{{ $event->name }}</h1>
 
-        <div class="flex items-center gap-4">
-            <img src="{{ Storage::disk('s3')->url($event->organization->user->profile_picture_url) }}" class="w-12 h-12 rounded-full object-cover" alt="">
-            <div class="flex flex-col">
-                <span class="text-sm text-gray-600">Dibuat oleh</span>
-                <span class="text-lg font-semibold">{{ $event->organization->user->name }}</span>
-            </div>
+        <div class="flex justify-between items-center gap-3">
+            <a href="{{ route('guest.organizations.show', ['id' => $event->organization_id]) }}" class="flex gap-4">
+                <img src="{{ Storage::disk('s3')->url($event->organization->user->profile_picture_url) }}" class="w-12 h-12 rounded-full object-cover" alt="">
+                <div class="flex flex-col">
+                    <span class="text-sm text-gray-600">Dibuat oleh</span>
+                    <span class="text-lg font-semibold">{{ $event->organization->user->name }}</span>
+                </div>
+            </a>
 
-            <div class="hidden md:flex items-center gap-2 ml-auto">
-                <a href="{{ route('guest.organizations.show', ['id' => $event->organization_id]) }}" class="px-6 py-2 border text-[var(--color1)] border-[var(--color1)]  rounded-md hover:bg-[var(--color1)] hover:text-white hover:shadow-md hover:scale-[1.05] transition duration-300 font-medium">
-                    Lihat
-                </a>
-                <a href="{{ route('login') }}" class="inline-block bg-[var(--color1)] border hover:bg-[var(--hovercolor1)] cursor-pointer border-[var(--color1)] text-white font-semibold py-2 px-6 rounded-lg shadow-md hover:scale-[1.05] transition duration-300">
-                    + Ikuti
-                </a>
-            </div>
+            <a href="{{ route('login') }}" class="inline-block bg-[var(--color1)] border hover:bg-[var(--hovercolor1)] cursor-pointer border-[var(--color1)] text-white font-semibold py-2 px-6 rounded-lg shadow-md hover:scale-[1.05] transition duration-300 flex-shrink-0">
+                + Ikuti
+            </a>
         </div>
     </section>
 
